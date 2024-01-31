a2309149af10e9a9861b8620d0a41b950d2a1cab
HongzeXiao <124698379+HongzeXiao@users.noreply.github.com>
2024-01-30 21:44:33 -0500

    submit

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -1,13 +1,37 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest){
     // fill here in
+    const comments = await prisma.comment.findMany({
+        take: 50,
+        orderBy: {
+            createdAt: 'desc'
+        }
+    });
 
-    return NextResponse.json({ test: "success", }, { status: 200 })
+    return NextResponse.json(comments);
+
+    // return NextResponse.json({ test: "success", }, { status: 200 })
 }
 
 export async function POST(req: NextRequest){
     // fill here in
-    return NextResponse.json({ test: "success", }, { status: 200 })
+
+    const { name, email, comment } = await req.json();
+
+    // Create a new comment in the database
+    const newComment = await prisma.comment.create({
+        data: {
+            name,
+            email,
+            comment
+        }
+    });
+
+    return NextResponse.json(newComment);
+    
+    // return NextResponse.json({ test: "success", }, { status: 200 })
 }
